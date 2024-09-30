772daca454efe91bc013a999ec6a72d95eeba52b
Jorge Reus <j-g1996@live.com>
2024-09-30 01:43:59 -0600

    feat: Added go back to last tag

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@ vim.keymap.set("n", "<C-q>", "<cmd>bdelete<cr>", { desc = "Close buffer" })
 vim.keymap.set("n", "<C-S-q>", "<cmd>BufferLineCloseOthers<cr>", { desc = "Close all other buffers" })
 vim.keymap.set("n", "<C-left>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Go to the previous buffer" })
 vim.keymap.set("n", "<C-right>", "<cmd>BufferLineCycleNext<cr>", { desc = "Go to the next buffer" })
+
+vim.keymap.set("n", "gt", "<C-t>", { desc = "Jump Back in the tag stack", noremap = true, silent = true })
